5165cd8a9f2fbff5648969f13f77e2013596c7a6
Giuliano De La Penna <delapenna.g@gmail.com>
2017-10-06 13:37:40 -0300

    Agrega funcionalidad para que pase el test de Project que chequea los campos del colaborador

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,11 +1,20 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :name, :users
+  attributes :id, :name
+  #attribute :users do
+    #object.users.zooniverseHandle
+    #tt = object.collaborations.each.select { | c | c.user.id; c.user.zooniverseHandle; c.points }
+   # byebug
+  #end
 
   has_many :collaborations
-  has_many :users, through: :collaborations
+  has_many :users, through: :collaborations do
+    #byebug
+    h= object.collaborations.collect { | c | [[:id,c.user.id],[:zooniverseHandle,c.user.zooniverseHandle],[:points,c.points]]}
+    h.map { |ar| ar.to_h }
+  end
 end
 
 #def users
-#  byebug
+ # byebug
 #  object.collaborations.users.map { |c| c.id; c.zooniverseHandle; c.points }
 #end
