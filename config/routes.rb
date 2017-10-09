afa73ed88b869ceeb0c3816502371d9dc2cef68d
Giuliano <delapenna.g@gmail.com>
2017-10-09 14:49:37 -0300

    Actualiza routes con lo que parece ser la config adecuada

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users do
+    resources :collaborations
+  end
   resources :projects do
-    resource :collaborations
+    resources :collaborations
   end
 end
